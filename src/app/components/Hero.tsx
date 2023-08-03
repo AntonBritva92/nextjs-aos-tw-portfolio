c2ce217ba9bfbbfd294f3484d33c24789ef98679
Anton Britva <destroev88@gmail.com>
2023-08-03 19:33:35 +0300

    first comment

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,21 @@
+import NavBar from "./NavBar";
+
+const Hero = () => {
+  return (
+    <div className="bg-[url()] lg:bg-[url(/banner_bg.jpg)] min-h-screen bg-cover">
+      <NavBar />
+      <div className="container grid lg:grid-cols-2 h-[clac(100vh-60px)]">
+        <div className="hidden lg:block"></div>
+        <div className="text-[100px] sm:text-[130px] font-bold leading-tight flex justify-center items-center">
+          <div>
+            <p data-aos="zoom-in-up">I'm</p>
+            <p data-aos="zoom-in-up">David</p>
+            <p data-aos="zoom-in-up">Smith</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
